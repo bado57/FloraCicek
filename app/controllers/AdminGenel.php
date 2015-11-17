87c49328b6eacf02db0ff393eb24fe02a981dea0
bayram <bayram@192.168.1.21>
2015-11-17 03:51:31 +0200

    bazı sayfa eksikleri giderildi saat 03:51 Signed-off-by:bayram <bayram@192.168.1.21>

--- app/controllers/AdminGenel.php
+++ app/controllers/AdminGenel.php
@@ -924,20 +924,17 @@ class AdminGenel extends Controller {
                                                     //oranlama
                                                     $width = $image->image_src_x;
                                                     $height = $image->image_src_y;
-                                                    /*
-                                                      $oran = $width / $height;
-                                                      if ($oran < 1) {
-                                                      $newheight = 600;
-                                                      $newwidth = round($height * $oran);
-                                                      } else if ($oran == 1) {
-                                                      $newheight = 600;
-                                                      $newwidth = 600;
-                                                      } else {
-                                                      $newheight = round($width / $oran);
-                                                      $newwidth = 600;
-                                                      }
-                                                     * 
-                                                     */
+                                                    $oran = $width / $height;
+                                                    if ($oran < 1) {
+                                                        $newheight = 500;
+                                                        $newwidth = round($height * $oran);
+                                                    } else if ($oran == 1) {
+                                                        $newheight = 500;
+                                                        $newwidth = 500;
+                                                    } else {
+                                                        $newheight = round($width / $oran);
+                                                        $newwidth = 500;
+                                                    }
                                                     if ($image->uploaded) {
                                                         // sadece resim formatları yüklensin
                                                         $image->allowed = array('image/*');
@@ -949,8 +946,8 @@ class AdminGenel extends Controller {
                                                         $image->file_name_body_pre = 'flora_';
                                                         $image->image_resize = true;
                                                         $image->image_ratio_crop = false;
-                                                        $image->image_x = $width;
-                                                        $image->image_y = $height;
+                                                        $image->image_x = $newwidth;
+                                                        $image->image_y = $newheight;
                                                         $image->image_watermark = 'images/watermark.png';
                                                         $image->image_watermark_position = 'B';
 
@@ -1100,20 +1097,18 @@ class AdminGenel extends Controller {
                                                 //oranlama
                                                 $width = $image->image_src_x;
                                                 $height = $image->image_src_y;
-                                                /*
-                                                  $oran = $width / $height;
-                                                  if ($oran < 1) {
-                                                  $newheight = 600;
-                                                  $newwidth = round($height * $oran);
-                                                  } else if ($oran == 1) {
-                                                  $newheight = 600;
-                                                  $newwidth = 600;
-                                                  } else {
-                                                  $newheight = round($width / $oran);
-                                                  $newwidth = 600;
-                                                  }
-                                                 * 
-                                                 */
+
+                                                $oran = $width / $height;
+                                                if ($oran < 1) {
+                                                    $newheight = 500;
+                                                    $newwidth = round($height * $oran);
+                                                } else if ($oran == 1) {
+                                                    $newheight = 500;
+                                                    $newwidth = 500;
+                                                } else {
+                                                    $newheight = round($width / $oran);
+                                                    $newwidth = 500;
+                                                }
                                                 if ($image->uploaded) {
                                                     // sadece resim formatları yüklensin
                                                     $image->allowed = array('image/*');
@@ -1125,8 +1120,8 @@ class AdminGenel extends Controller {
                                                     $image->file_name_body_pre = 'flora_';
                                                     $image->image_resize = true;
                                                     $image->image_ratio_crop = false;
-                                                    $image->image_x = $width;
-                                                    $image->image_y = $height;
+                                                    $image->image_x = $newwidth;
+                                                    $image->image_y = $newheight;
                                                     $image->image_watermark = 'images/watermark.png';
                                                     $image->image_watermark_position = 'B';
 
@@ -1467,20 +1462,17 @@ class AdminGenel extends Controller {
                                                     //oranlama
                                                     $width = $image->image_src_x;
                                                     $height = $image->image_src_y;
-                                                    /*
-                                                      $oran = $width / $height;
-                                                      if ($oran < 1) {
-                                                      $newheight = 600;
-                                                      $newwidth = round($height * $oran);
-                                                      } else if ($oran == 1) {
-                                                      $newheight = 600;
-                                                      $newwidth = 600;
-                                                      } else {
-                                                      $newheight = round($width / $oran);
-                                                      $newwidth = 600;
-                                                      }
-                                                     * 
-                                                     */
+                                                    $oran = $width / $height;
+                                                    if ($oran < 1) {
+                                                        $newheight = 500;
+                                                        $newwidth = round($height * $oran);
+                                                    } else if ($oran == 1) {
+                                                        $newheight = 500;
+                                                        $newwidth = 500;
+                                                    } else {
+                                                        $newheight = round($width / $oran);
+                                                        $newwidth = 500;
+                                                    }
                                                     if ($image->uploaded) {
                                                         // sadece resim formatları yüklensin
                                                         $image->allowed = array('image/*');
@@ -1492,8 +1484,8 @@ class AdminGenel extends Controller {
                                                         $image->file_name_body_pre = 'flora_';
                                                         $image->image_resize = true;
                                                         $image->image_ratio_crop = false;
-                                                        $image->image_x = $width;
-                                                        $image->image_y = $height;
+                                                        $image->image_x = $newwidth;
+                                                        $image->image_y = $newheight;
                                                         $image->image_watermark = 'images/watermark.png';
                                                         $image->image_watermark_position = 'B';
 
@@ -1732,20 +1724,18 @@ class AdminGenel extends Controller {
                                                 //oranlama
                                                 $width = $image->image_src_x;
                                                 $height = $image->image_src_y;
-                                                /*
-                                                  $oran = $width / $height;
-                                                  if ($oran < 1) {
-                                                  $newheight = 600;
-                                                  $newwidth = round($height * $oran);
-                                                  } else if ($oran == 1) {
-                                                  $newheight = 600;
-                                                  $newwidth = 600;
-                                                  } else {
-                                                  $newheight = round($width / $oran);
-                                                  $newwidth = 600;
-                                                  }
-                                                 * 
-                                                 */
+
+                                                $oran = $width / $height;
+                                                if ($oran < 1) {
+                                                    $newheight = 500;
+                                                    $newwidth = round($height * $oran);
+                                                } else if ($oran == 1) {
+                                                    $newheight = 500;
+                                                    $newwidth = 500;
+                                                } else {
+                                                    $newheight = round($width / $oran);
+                                                    $newwidth = 500;
+                                                }
                                                 if ($image->uploaded) {
                                                     // sadece resim formatları yüklensin
                                                     $image->allowed = array('image/*');
@@ -1757,8 +1747,8 @@ class AdminGenel extends Controller {
                                                     $image->file_name_body_pre = 'flora_';
                                                     $image->image_resize = true;
                                                     $image->image_ratio_crop = false;
-                                                    $image->image_x = $width;
-                                                    $image->image_y = $height;
+                                                    $image->image_x = $newwidth;
+                                                    $image->image_y = $newheight;
                                                     $image->image_watermark = 'images/watermark.png';
                                                     $image->image_watermark_position = 'B';
 
